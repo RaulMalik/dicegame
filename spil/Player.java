7a7d01844c794a84449168fb3546e972bc1abdea
HamailAzam <hamailazam@gmail.com>
2024-10-09 12:12:42 +0200

    private get and sets for PlayerName and PlayerScore.

--- spil/Player.java
+++ spil/Player.java
@@ -1,3 +1,21 @@
 public class Player {
+    private String playerName;
+    private int PlayerScore = 0;
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public void setPlayerName(String playerName) {
+        this.playerName = playerName;
+    }
+
+    public int getPlayerScore() {
+        return PlayerScore;
+    }
+
+    public void setPlayerScore(int playerScore) {
+        this.PlayerScore = playerScore;
+    }
 
 }
